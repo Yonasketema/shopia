54122dd9516abf1213e6af7828609e16334f008e
yonasketema <yonask.kitaw@gmail.com>
2023-05-06 16:49:21 +0300

    fix: loading state error

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -3,6 +3,7 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Product {
+  id: number;
   title: string;
   price: number;
   image_uri: string;
@@ -11,21 +12,28 @@ export interface Product {
 const useProduct = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     apiClient
       .get<Product[]>("/product", { signal: controller.signal })
-      .then((res) => setProducts(res.data))
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { products, error };
+  return { products, error, isLoading };
 };
 
 export default useProduct;
