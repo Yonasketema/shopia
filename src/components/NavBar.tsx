4309c422f25a7ebbf67539b4de966c241bc5abfa
yonasketema <yonask.kitaw@gmail.com>
2023-05-06 15:13:50 +0300

    add Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import { HStack, Text } from "@chakra-ui/react";
+
+function NavBar() {
+  return (
+    <HStack padding={2} fontWeight="bold">
+      <Text fontSize="2xl">Shopia</Text>
+    </HStack>
+  );
+}
+
+export default NavBar;
