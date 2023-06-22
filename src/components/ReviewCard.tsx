e7c949de550627e460983409514ec09184ac6e40
yonasketema <yonask.kitaw@gmail.com>
2023-06-22 17:12:43 +0300

    style:comment profile

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -13,22 +13,12 @@ const ReviewCard = ({ review }: Prop) => {
       paddingY={6}
       gap={3}
     >
-      <Image
-        src={
-          "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
-        }
-        width={50}
-        height={50}
-        fit="cover"
-        borderRadius="50%"
-        borderWidth={2}
-        borderColor={"gray.400"}
-      />
+       
       <Box>
         <Text fontSize="larger" fontWeight="black" marginBottom={1}>
           {review.name}
         </Text>
-        <Text fontSize="sm" color={"gray.600"}>
+        <Text fontSize="sm" color={"gray.600"} marginLeft={3}>
           {review.description}
         </Text>
       </Box>
